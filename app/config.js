774feffe6809a6134e46b6d51bd0106adcead4b1
ntshuxeko7lll-jpg <ntshu.xeko7lll@gmail.com>
2025-12-31 22:43:48 +0200

    Add application configuration for LearnSphere

--- app/config.js
+++ app/config.js
@@ -1,1 +1,13 @@
 
+const APP_CONFIG = {
+  appName: "LearnSphere",
+  grades: [8, 9, 10, 11, 12],
+  subjects: [
+    "Mathematics",
+    "Physical Science",
+    "Life Sciences",
+    "English",
+    "Geography",
+    "History"
+  ]
+};
