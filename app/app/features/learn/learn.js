fbe7a57013118b4c275969948b4cd37bebd22326
ntshuxeko7lll-jpg <ntshu.xeko7lll@gmail.com>
2025-12-31 18:55:58 +0200

    Add LearnScreen function for learning interface

--- app/app/features/learn/learn.js
+++ app/app/features/learn/learn.js
@@ -1,1 +1,10 @@
+function LearnScreen() {
+  return `
+    <div class="screen">
+      <h2>Learn</h2>
+      <p>Select a grade and subject.</p>
 
+      <pre>Curriculum will load here.</pre>
+    </div>
+  `;
+}
