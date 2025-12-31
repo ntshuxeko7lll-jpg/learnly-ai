c1b95932511186439db5c376753d1bfaae09a8e1
ntshuxeko7lll-jpg <ntshu.xeko7lll@gmail.com>
2025-12-31 18:54:32 +0200

    Add HomeScreen component for learning app

--- app/app/features/home/home.js
+++ app/app/features/home/home.js
@@ -1,1 +1,12 @@
+function HomeScreen() {
+  return `
+    <div class="screen">
+      <h1>LearnSphere</h1>
+      <p>High School Learning App (Grades 8–12)</p>
 
+      <button onclick="router.navigate('learn')">
+        Start Learning
+      </button>
+    </div>
+  `;
+}
